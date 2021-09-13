9a88c88f873fb4b7aa95bee7f6ef40b5b268870b
Antony Agrios <antonyagrios@gmail.com>
2021-09-13 20:05:26 +0300

    feat(chrome): Add typescriptreact and javascriptreact debug support (chrome)

---
+++ lua/dap-install/core/debuggers/chrome.lua
@@ -0,0 +1,44 @@
+local M = {}
+
+local dbg_path = require("dap-install.config.settings").options["installation_path"] .. "vscode-chrome-debug/"
+
+M.dap_info = {
+	name_adapter = "node2",
+	name_configuration = "javascript",
+}
+
+M.config = {
+	adapters = {
+		type = "executable",
+		command = "node",
+		args = {dbg_path .. "/path/to/vscode-chrome-debug/out/src/chromeDebug.js"}
+	},
+	configurations = {
+		{
+			type = "chrome",
+			request = "attach",
+			program = "${file}",
+			cwd = vim.fn.getcwd(),
+			sourceMaps = true,
+			protocol = "inspector",
+			port = 9222,
+			webRoot = "${workspaceFolder}"
+		},
+	},
+}
+
+M.installer = {
+	before = "",
+	install = [[
+		git clone https://github.com/Microsoft/vscode-chrome-debug && cd vscode-chrome-debug
+		npm install
+		npm run build
+	]],
+	uninstall = [[
+		cd vscode-chrome-debug && npm uninstall .
+		cd ../..
+		rm -rf vscode-chrome-debug
+	]],
+}
+
+return M
